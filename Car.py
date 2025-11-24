9e921d8f67e14a5cff1dbc919e6512d44836af12
Shalev Mashiah <shalev10568@gmail.com>
2025-11-24 13:53:07 +0200

    adding a init function to the vehicle and car class

--- Car.py
+++ Car.py
@@ -1,6 +1,10 @@
 # Car.py
 
 class Car(Vehicle):
+
+    def __init__(self):
+        super().__init__("Car")
+        
     def start_engine(self):
         print("Car Engine started ===> Vroom Vroom!")
 
--- Vehicle.py
+++ Vehicle.py
@@ -3,6 +3,11 @@
 from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
+
+    def __init__(self, vehicle_type):
+        self.make = vehicle_type
+
+
     @abstractmethod
     def start_engine(self):
         pass
