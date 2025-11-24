4f9455ca5e30c25b048c2bc988b56a03a8655d5e
Shalev Mashiah <shalev10568@gmail.com>
2025-11-24 14:45:30 +0200

    adding template files, checking the project in the main

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# main.py
+
+
+from car import Car
+from truck import Truck
+from motorcycle import Motorcycle
+
 def main():
-    print("Hello, World!")
+    V1 = Car()
+    V2 = Truck()
+    V3 = Motorcycle()
+    V1.start_engine()
+    V2.start_engine()
+    V3.start_engine()
 
 if __name__ == "__main__":
     main()
