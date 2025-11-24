71c942d2131248adecdf4542378eb66e95403609
Shalev Mashiah <shalev10568@gmail.com>
2025-11-24 13:54:55 +0200

    Truck class

---
+++ Truck.py
@@ -0,0 +1,10 @@
+# Car.py
+
+class Truck(Vehicle):
+
+    def __init__(self):
+        super().__init__("Truck")
+        
+    def start_engine(self):
+        print("Truck Engine started ===> Vroom Vroom!")
+
